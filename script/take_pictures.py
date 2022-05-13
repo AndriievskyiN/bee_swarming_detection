d193fe3e50079e0d970fda22b7c2bd80ecbf17f9
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-05-13 14:08:55 +0200

    slight change

--- script/take_pictures.py
+++ script/take_pictures.py
@@ -7,7 +7,7 @@ cam = cv2.VideoCapture(0)
 
 # Reading the image from the camera and saving it to our clear_sky folder
 s, img = cam.read()
-cv2.imwrite(f"/home/nikita/bee_swarming/images/clear_sky/{datetime.now()}.jpg", img)
+cv2.imwrite(f"images/clear_sky/{datetime.now()}.jpg", img)
 
 # Releasing the camera, so that we don't capture video all the time
 cam.release()
