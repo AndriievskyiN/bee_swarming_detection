56013fcbef56be9f9ac60edcb6d4a821152bf947
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-05-24 09:59:38 +0200

    new photos

---
+++ script/save_to_github.py
@@ -0,0 +1,8 @@
+import os
+
+# Pushing changes to github at the end of the day
+def pushToGitHub(current_time):
+    if current_time.strftime("%H:%M") > "09:00":
+        os.system("git add .")
+        os.system('git commit -m "new photos"')
+        os.system("git push -u origin main")
