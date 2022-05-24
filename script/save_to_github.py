673351b2d7213103f518ae01f73ba07cc6051c57
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-05-24 12:24:01 +0200

    pushing to github from 21 to 21:03

--- script/save_to_github.py
+++ script/save_to_github.py
@@ -4,7 +4,8 @@ from datetime import datetime as dt
 
 # Pushing changes to github at the end of the day
 def pushToGitHub(current_time):
-    if current_time.strftime("%H:%M") > "11:00":
-        os.system("git add .")
-        os.system('git commit -m "new photos"')
-        os.system("git push -u origin main")
+    if current_time.strftime("%H:%M") > "21:00":
+        if current_time.strftime("%H:%M") < "21:03":
+            os.system("git add .")
+            os.system('git commit -m "new photos"')
+            os.system("git push -u origin main")
