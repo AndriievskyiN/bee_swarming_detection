940a1d0cc84d9e46c8d639d0b0f5744052fa9ebb
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-05-24 09:40:39 +0200

    Not taking pictures if it's later than 9pm

--- script/take_pictures.py
+++ script/take_pictures.py
@@ -1,13 +1,18 @@
 # Importing libraries
 import cv2
-from datetime import datetime
+from datetime import date, datetime
 
-# Initializing the camera
-cam = cv2.VideoCapture(0)
 
-# Reading the image from the camera and saving it to our clear_sky folder
-s, img = cam.read()
-cv2.imwrite(f"images/clear_sky/{datetime.now()}.jpg", img)
+# Reading the image from the camera and saving it to our clear_sky folder if it's not later than 9 PM
 
-# Releasing the camera, so that we don't capture video all the time
-cam.release()
+now = datetime.now()
+if now.strftime("%H:%M") < "21:00":
+
+    # Initializing the camera
+    cam = cv2.VideoCapture(0)
+
+    s, img = cam.read()
+    cv2.imwrite(f"images/clear_sky/{datetime.now()}.jpg", img)
+
+    # Releasing the camera, so that we don't capture video all the time
+    cam.release()
