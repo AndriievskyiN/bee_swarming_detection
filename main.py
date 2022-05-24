b822d821952a0e2bf4f2dbad7487b502528b0ac4
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-05-24 12:19:35 +0200

    new photos

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from script import cv2, datetime, os
 from script.take_pictures import take_pictures
 from script.save_to_github import pushToGitHub
+import sqlite3
 
 current_time = datetime.now()
+conn = sqlite3.connect("pushed.db")
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS pushed (
+    wasPushed INT
+)''')
+conn.commit()
+
 
 if __name__ == "__main__":
     take_pictures(current_time)
-    pushToGitHub(current_time)
+    cur.execute("SELECT wasPushed FROM pushed")
+    try:
+        wasPushed = cur.fetchone()[0]
+    except:
+        pushToGitHub(current_time)
+        cur.execute('''INSERT INTO pushed 
+                        VALUES (1)''')
+        conn.commit()
--- script/save_to_github.py
+++ script/save_to_github.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 
 # Pushing changes to github at the end of the day
 def pushToGitHub(current_time):
-    if current_time.strftime("%H:%M") > "09:00":
+    if current_time.strftime("%H:%M") > "11:00":
         os.system("git add .")
         os.system('git commit -m "new photos"')
         os.system("git push -u origin main")
