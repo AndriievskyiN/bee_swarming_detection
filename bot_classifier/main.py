ae877f286ec478f54a169a16f407a199c2219553
Nikita Andriievskyi <n.andrievskiy@gmail.com>
2022-06-14 16:35:32 +0200

    final commit

---
+++ bot_classifier/main.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import cv2
+
+import os
+import imghdr
+from datetime import datetime
+
+from predict import predict_bee_swarming
+from email_sender import send_email
+from take_pictures import take_pictures
+
+model = tf.keras.models.load_model("models/model2.h5")
+current_time = datetime.now()
+
+if __name__ == "__main__":
+    # Take pictures
+    img_path = take_pictures(current_time)
+    # Predict bee swarming
+    prediction = predict_bee_swarming(img_path, model)
+    if not prediction:
+        #Send email
+        send_email(img_path)
+    else:
+        pass
+        # Delete the image
+        #os.remove(img_path)
+
